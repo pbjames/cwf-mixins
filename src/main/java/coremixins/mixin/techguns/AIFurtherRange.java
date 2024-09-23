597e9f7fd17a516940392f07f63b1fcaa25abdd8
James <jameskowal10@gmail.com>
2024-09-23 12:15:59 +0100

    Extend AI range for techguns (needs testing)

--- src/main/java/coremixins/mixin/techguns/AIFurtherRange.java
+++ src/main/java/coremixins/mixin/techguns/AIFurtherRange.java
@@ -1,5 +1,16 @@
 package coremixins.mixin.techguns;
 
+import org.spongepowered.asm.mixin.Mixin;
+import techguns.TGuns;
+
+@Mixin(TGuns.class)
+public abstract class AIFurtherRange {
+  private static final float RANGE_CLOSE = 12.0f;
+  private static final float RANGE_SHORT = 24.0f;
+  private static final float RANGE_MEDIUM = 48.0f;
+  private static final float RANGE_FAR = 80.0f;
+}
+
 // @Mixin(GenericProjectile.class)
 // public abstract class AIFurtherRange extends Entity {
 //  private BulletsBreakGlass(World worldIn) {
